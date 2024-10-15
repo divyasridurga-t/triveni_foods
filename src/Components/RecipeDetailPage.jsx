371d7105d7363bf61d8c3bbb4f42d579717dccb4
Divya Sri <divyasridurga.t@engro.io-at-491938176112>
2024-10-16 00:00:08 +0530

    whatsapp redirection

--- src/Components/RecipeDetailPage.jsx
+++ src/Components/RecipeDetailPage.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import Layout from "./Layout";
 import { useParams } from "react-router-dom";
 
 const RecipeDetailPage = () => {
   let { recipe = "", id = "" } = useParams();
+  let [more, setMore] = useState(false);
+
+  function handleChange(e) {
+    console.log();
+    if (e.target.value === "more") {
+      setMore(true);
+    }
+  }
+
+  function placeOrder() {
+    let message = "hello lokeshhh !!"; // Your message
+    let encoded_msg = encodeURIComponent(message); // Encode the message
+    let phoneNumber = "918985755632"; // Phone number in international format without symbols
+
+    // Construct the WhatsApp URL
+    let url = `https://wa.me/${phoneNumber}?text=${encoded_msg}`;
+
+    // Redirect to the WhatsApp URL
+    window.open(url, '_blank'); // Use '_blank' to open in a new tab
+  }
 
   return (
     <>
@@ -20,16 +41,30 @@ const RecipeDetailPage = () => {
             <div className="form_">
               <input className="type_text" value={recipe} type="text" />
               <div>
-                <select name="" id="">
+                <select onChange={handleChange} name="" id="">
                   <option>Choose quantity</option>
                   <option>1/4 kg</option>
                   <option>1/2 kg</option>
                   <option>1 kg</option>
                   <option>2 kg</option>
                   <option>3 kg</option>
-                  <option>more...</option>
+                  <option value={"more"}>more...</option>
                 </select>
-                <input className="type_text" type="number"/>
+                {more && (
+                  <input className="type_text" type="number" min="4" step="1" />
+                )}
+              </div>
+              <div>
+                <input
+                  placeholder="customizations if any"
+                  className="type_text"
+                  type="text"
+                />
+              </div>
+              <div>
+                <button onClick={placeOrder} className="place_order">
+                  Place Order
+                </button>
               </div>
             </div>
           </div>
