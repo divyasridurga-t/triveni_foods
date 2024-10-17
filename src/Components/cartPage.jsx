ff983b26ba659686fe53d9cd220121e42a7337e7
LokeshMohanMahankali <lokeshmahankali@gmail.com>
2024-10-17 23:07:37 +0530

    changes

---
+++ src/Components/cartPage.jsx
@@ -0,0 +1,16 @@
+import Layout from "./Layout";
+
+const CartPage = () => {
+  return (
+    <>
+      <Layout>
+        <div className="cart_page">
+           <div className="cart_card">
+            </div> 
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default CartPage;
